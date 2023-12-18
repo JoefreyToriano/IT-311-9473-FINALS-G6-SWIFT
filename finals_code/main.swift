3c77ddeed9fb6bd4fc6fb4a4c9264cdb73442649
Joefrey Dionisio Toriano <113817799+JoefreyToriano@users.noreply.github.com>
2023-12-18 22:03:48 +0800

    Create main.swift
    Initialize main.swift code

---
+++ finals_code/main.swift
@@ -0,0 +1,22 @@
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+/*
+ IT 311 | 9473 - Applications Development
+ 10:30-11:30 AM | 12:1:30 PM TF
+ 
+ GROUP 6
+ ANGWAY, Khenler
+ ANGWAY, Timothy Jue
+ AZURIN, John Arvin Dale
+ CLEMENTE, Lise
+ HUFALAR, Jekka Menna
+ GANGAN, William Sylvester
+ TORIANO, Joefrey
+ */
+
+/*
+ Aim: Create an informative console app named BAGUINFORM
+ Target Users: Baguio citizens and tourists
+ Purpose: Provide helpful contact information and tourist destination details
+ */
+
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
