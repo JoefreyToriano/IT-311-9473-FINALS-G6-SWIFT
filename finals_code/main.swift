a632dc4be8e59608d0b6e4eb0e7da53b7a9745c8
Timothy Angway <144763672+Timothy2215221@users.noreply.github.com>
2023-12-19 21:48:35 +0800

    added sign up function

--- finals_code/main.swift
+++ finals_code/main.swift
@@ -20,3 +20,48 @@
  */
 
 // -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+
+// signUp() function
+func signUp() {
+print("                                       ")
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("*               Sign Up for an Account              *")
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("*                                                   *")
+    print("*          Please enter the following details       *")
+print("*     - - - - - - - - - - - - - - - - - - - - -     *")
+print("*                                                   *")
+print("           First Name:", terminator: " ")
+let fName = readLine() ?? ""            // read user input for First Name
+
+print("           Last Name:", terminator: " ")
+let lName = readLine() ?? ""            // read user input for Last Name
+
+    print("           Address:", terminator: " ")
+let addr = readLine() ?? ""            // read user input for Address
+
+print("           Username:", terminator: " ")
+let user = readLine() ?? ""            // read user input for Username
+
+print("           Password:", terminator: " ")
+let pass = readLine() ?? ""            // read user input for Password
+
+// add a new user to the Account()
+let newUser = Account(username: user, password: pass,
+firstName: fName, lastName: lName,
+address: addr)
+
+    // add user inputs to the Accounts list
+accounts.append(newUser)
+
+// print success message
+print("                                                     ")
+    print("*     - - - - - - - - - - - - - - - - - - - - -     *")
+print("*          Congratulations! Account created!        *")
+    print("*              Please sign in your account.         *")
+  print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    print("*                                                   *")
+    
+    // return to main menu
+    initialDis()
+} // end of func Account()
